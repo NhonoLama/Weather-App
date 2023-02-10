d4d66eeb7755c4b716561d51e0d9c1816cf1a965
NhonoLama <wadalama111@gmail.com>
2023-02-10 22:04:53 +0545

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,34 @@
 const form = document.querySelector('form');
+const details = document.querySelector('.details');
+const card = document.querySelector('.card');
+
+const updateUI = async (data) => {
+    //const cityDetails = data.cityDetails;
+    //const weather = data.weather;
+
+    //destructing property
+    const {cityDetails , weather} = data;    // shorter form of above statements
+    
+    details.innerHTML = ` <div class="details text-muted text-uppercase text-center">
+          <h5 class="my-3">${cityDetails.LocalizedName}</h5>
+          <div class="my-3">${weather.WeatherText}</div>
+          <div class="display-4 my-4">
+              <span>${weather.Temperature.Metric.Value}</span>
+              <span>&deg;C</span>
+          </div>
+      </div>`
+    
+    if(card.classList.contains('d-none')){
+      card.classList.remove('d-none');
+    }
+}
 
 const updateCity = async (city) => {
 
     const cityDetails = await getCity(city);
     const weather = await getWeather(cityDetails.Key);
 
-    return {
-        cityDetails : cityDetails,
-        weather : weather
-    }
+    return { cityDetails , weather};    //object shorthand notation  i.e. { cityDetails : cityDetails , weather : weather }
 }
 
 form.addEventListener('submit', e => {
@@ -18,7 +38,7 @@ form.addEventListener('submit', e => {
   form.reset();
   
   updateCity(city)
-  .then(data => {console.log(data)})
+  .then(data => updateUI(data))
   .catch(err => {console.log(err)})
 })
 
